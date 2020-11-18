98b9c95bcbb1460a6a748cac8ea4566ac115a441
Bùi Văn Hiệu <buivanhieu2105@gmail.com>
2020-11-18 18:31:10 +0700

    -add framework

---
+++ NetaloUISDK.framework/Headers/NetaloUISDK.h
@@ -0,0 +1,19 @@
+//
+//  NetaloUISDK.h
+//  NetaloUISDK
+//
+//  Created by Tran Phong on 7/22/20.
+//  Copyright © 2020 'Netalo'. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetaloUISDK.
+FOUNDATION_EXPORT double NetaloUISDKVersionNumber;
+
+//! Project version string for NetaloUISDK.
+FOUNDATION_EXPORT const unsigned char NetaloUISDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetaloUISDK/PublicHeader.h>
+
+
